4f05f65b906ab7c74c32c86967f4d6ee8d8e1190
HeskeyBaozi <hezhiyu233@foxmail.com>
2018-06-06 11:33:13 +0800

    global scss vars

---
+++ vue.config.js
@@ -0,0 +1,31 @@
+const merge = require('webpack-merge');
+
+module.exports = {
+  chainWebpack: config => {
+    config.module
+      .rule('ts')
+      .use('ts-loader')
+      .tap(options =>
+        merge(options, {
+          getCustomTransformers: () => ({
+            before: [
+              require('ts-import-plugin')({
+                libraryName: 'element-ui',
+                styleLibraryName: 'theme-chalk'
+              })
+            ]
+          })
+        })
+      );
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        data: fs.readFileSync('src/global-variables.scss', 'utf-8')
+      }
+    }
+  },
+  pwa: {
+    name: 'matrix-online-judge'
+  }
+};
