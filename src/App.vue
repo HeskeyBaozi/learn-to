be75adb1122f69b463ce862883d11120ba004af1
HeskeyBaozi <hezhiyu233@foxmail.com>
2018-07-21 15:06:08 +0800

    basic layout

--- src/App.vue
+++ src/App.vue
@@ -4,28 +4,27 @@
       <el-header class="header">
         <top-nav></top-nav>
       </el-header>
-      <el-main class="main">
-        <router-view></router-view>
-      </el-main>
-      <el-footer>
-        <div>
-          <p>现在是 {{ Date.now() | format }}</p>
-          <p>2017年10月1日距离现在 {{ Date.now() | formatDistance(new Date(2017, 10, 1)) }}</p>
-        </div>
-      </el-footer>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+          <el-container>
+            <el-main class="main">
+              <router-view></router-view>
+            </el-main>
+            <el-footer>
+              <div>
+                <p>现在是 {{ Date.now() | format }}</p>
+                <p>2017年10月1日距离现在 {{ Date.now() | formatDistance(new Date(2017, 10, 1)) }}</p>
+              </div>
+            </el-footer>
+           </el-container>
+      </el-container>
     </el-container>
   </div>
 </template>
 
 <script lang="ts">
-import { RootStore } from '@/stores';
-import { IState, ITodo } from '@/stores/modules/example';
-import { FETCH_TODO_LIST, FINISHED_LIST } from '@/stores/modules/example';
 import TopNav from '@/views/global/TopNav.vue';
 import { Component, Vue } from 'vue-property-decorator';
-import { namespace } from 'vuex-class';
-
-const moduled = namespace('todos');
 
 @Component({
   name: 'app',
@@ -34,36 +33,6 @@ const moduled = namespace('todos');
   }
 })
 export default class App extends Vue {
-  // 获取状态
-  get todos() {
-    return (this.$store as RootStore).state.todos.todos;
-  }
-
-  get finishedTodos(): ITodo[] {
-    return (this.$store as RootStore).getters[`todos/${FINISHED_LIST}`];
-  }
-
-  // 获取状态 第二种写法
-  @moduled.State((state: IState) => state.todos)
-  todos2!: ITodo[];
-
-  @moduled.Getter(FINISHED_LIST) finishedTodos2!: ITodo[]; // 感叹号代表赋值断言，此值不为空
-
-  // 发送Action
-  @moduled.Action(FETCH_TODO_LIST) fetchTodos!: (payload: any) => Promise<void>;
-
-  async mounted() {
-
-    // 也是发送Action的写法
-    await this.$store.dispatch(
-      `todos/${FETCH_TODO_LIST}`,
-      { page: 1 } /* payload */
-    );
-
-    await this.fetchTodos('example_payload');
-    // tslint:disable-next-line:no-console
-    console.log(this.todos);
-  }
 }
 </script>
 
--- src/views/global/TopNav.vue
+++ src/views/global/TopNav.vue
@@ -3,29 +3,16 @@
     <div class="logo-wrapper" @click="backToHome">
       MOJ Logo
     </div>
-    <el-menu id="menu" mode="horizontal" :default-active="$route.path" :router="true">
-      <el-menu-item index="/problems">
-        <fa-icon :icon="['far', 'list-alt']"></fa-icon>
-        <span class="menu-item-text">题库</span>
-      </el-menu-item>
-      <el-menu-item index="/ranks">
-        <fa-icon icon="signal"></fa-icon>
-        <span class="menu-item-text">排名</span>
-      </el-menu-item>
-    </el-menu>
   </el-container>
 </template>
 
 <script lang="ts">
-import { format } from '@/utils/format';
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component({
   name: 'top-nav'
 })
 export default class TopNav extends Vue {
-  @Prop() private msg!: string;
-
   backToHome() {
     this.$router.push('/');
   }
