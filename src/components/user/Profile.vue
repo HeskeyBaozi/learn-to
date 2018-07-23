ebc48fb0d03a7d3face6d642e1064b6320e0fe31
EmilyBlues <liaolei3@mail2.sysu.edu.cn>
2018-07-23 17:13:13 +0800

    加入个人信息ElementUII卡片

---
+++ src/components/user/Profile.vue
@@ -0,0 +1,42 @@
+<template>
+<el-card class="box-card">
+  <div slot="header" class="clearfix">
+    <span>个人信息</span>
+    <el-button style="float: right; padding: 3px 0" type="primary">编辑</el-button>
+  </div>
+  <div v-for="o in 3" :key="o" class="text-item">
+    {{'列表内容 ' + o }}
+  </div>
+</el-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'profile'
+})
+export default class Profile extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+
+  .clearfix:after {
+    clear: both
+  }
+
+  .text-item{
+    font-size: 14px;
+    margin-bottom: 18px;
+  }
+
+  .box-card{
+    width: 480px;
+  }
+
+</style>
--- src/views/space/UserInformation.vue
+++ src/views/space/UserInformation.vue
@@ -1,15 +1,23 @@
 <template>
-  <el-container id="user-information">
-    <h1>个人信息页</h1>
-  </el-container>
+  <div>
+    <el-container id="user-information">
+      <h1>个人信息页</h1>
+    </el-container>
+    <profile></profile>
+  </div>
 </template>
 
 <script lang="ts">
+import Profile from '@/components/user/Profile.vue';
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component({
-  name: 'user-information'
+  name: 'user-information',
+  components: {
+    Profile
+  }
 })
+
 export default class UserInformation extends Vue {}
 </script>
 
