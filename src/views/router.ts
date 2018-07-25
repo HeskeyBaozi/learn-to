49505b763c89bc3e3871cab25b9b925f4426e87b
HeskeyBaozi <hezhiyu233@foxmail.com>
2018-07-25 22:15:45 +0800

    update router

--- src/views/router.ts
+++ src/views/router.ts
@@ -57,25 +57,26 @@ export default new Router({
           ]
         },
         {
-          path: 'space',
-          component: () => import('./space/User.vue'),
+          path: 'user',
+          component: () => import('./user/User.vue'),
           children: [
             {
               path: '',
               name: 'information',
-              component: () => import('./space/UserInformation.vue')
-            },
-            {
-              path: 'statistic',
-              name: 'statistic',
-              component: () => import('./space/UserStatistic.vue')
+              component: () => import('./user/UserInformation.vue')
             }
           ]
         },
         {
-          path: 'ranks',
-          name: 'ranks',
-          component: () => import('./TheRanks.vue')
+          path: 'statistic',
+          component: () => import('./statistic/Statistic.vue'),
+          children: [
+            {
+              path: 'ranks',
+              name: 'ranks',
+              component: () => import('./statistic/TheRanks.vue')
+            }
+          ]
         },
         {
           path: 'g2-example',
---
+++ src/views/statistic/Statistic.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="statistic">
+     统计数字 自行设置子路由
+     <router-view></router-view>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'statistic'
+})
+export default class Statistic extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+#statistic {
+}
+</style>
+
